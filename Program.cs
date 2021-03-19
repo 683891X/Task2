244e0511f27411b15df49ee260f891c17b0262a9
683891X <683891X@student.swin.edu.au>
2021-03-19 17:30:23 +1100

    Exercise one task two

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String gender;
+            int age;
+
+            Console.WriteLine("Please enter your gender: ");
+            gender = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age");
+            age = int.Parse(Console.ReadLine());
+
+            if(age > 17){
+                Console.WriteLine(gender + " is an adult");
+            }else{
+                Console.WriteLine(gender + " is not an adult");
+            }
+
+        }
+    }
+}
